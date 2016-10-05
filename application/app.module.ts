0fa7bb46df1477c09027a12fb6379d4c1f78930f
ChandraShekher Polimera <chandrashekher@hotmail.com>
2016-10-05 23:32:11 +0530

    Add files via upload

---
+++ application/app.module.ts
@@ -0,0 +1,29 @@
+import './rxjs-extensions';
+
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppComponent } from './components/header/header';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
+  ],
+  declarations: [
+    AppComponent
+  ],
+  providers: [
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {
+}
---
+++ application/components/header/header.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'application',
+  templateUrl: 'application/components/header/partials/views.html',
+  styleUrls: ['application/components/header/header.css']
+})
+export class AppComponent {
+  title = 'Angular2 Demo App';
+}
